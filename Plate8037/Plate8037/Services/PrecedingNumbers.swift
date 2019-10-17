5b902034fd2dbf152b56eca88ef6c1c57f8c0d50
Serge Sukhanov <ssuhanov@gmail.com>
2019-10-17 14:01:58 +0300

    Implement preceding numbers

---
+++ Plate8037/Plate8037/Services/PrecedingNumbers.swift
@@ -0,0 +1,37 @@
+//
+//  PrecedingNumbers.swift
+//  Plate8037
+//
+//  Created by Serge Sukhanov on 10/17/2019.
+//  Copyright © 2019 Serge Sukhanov. All rights reserved.
+//
+
+import Foundation
+
+class PrecedingNumbers {
+    func getNumbers(for digit: Int) -> [Int] {
+        var sign = -1
+        var addition = 0
+        var addedNumber = digit - 5
+        
+        var result: [Int] = []
+        
+        for _ in 0..<10 {
+            var nextNumber = addedNumber + sign * addition
+            while nextNumber < 0 {
+                nextNumber += 10
+            }
+            
+            while nextNumber >= 10 {
+                nextNumber -= 10
+            }
+            
+            result.append(nextNumber)
+            addedNumber = nextNumber
+            
+            addition += 1
+            sign = -sign
+        }
+        return result
+    }
+}
