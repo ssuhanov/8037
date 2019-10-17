64a1603d521a0406e5856bd6da155f2e8e524b14
Serge Sukhanov <ssuhanov@gmail.com>
2019-10-17 17:43:06 +0300

    Add result checking and simple animation

---
+++ Plate8037/Plate8037/Services/ResultChecker.swift
@@ -0,0 +1,21 @@
+//
+//  ResultChecker.swift
+//  Plate8037
+//
+//  Created by Serge Sukhanov on 10/17/2019.
+//  Copyright © 2019 Serge Sukhanov. All rights reserved.
+//
+
+import Foundation
+
+class ResultChecker {
+    var digits: (Int, Int, Int, Int)
+    
+    init(digits: (Int, Int, Int, Int)) {
+        self.digits = digits
+    }
+    
+    func correctAnswer() -> Int {
+        return digits.0 * 10 + digits.1 + digits.2 * 10 + digits.3
+    }
+}
--- Plate8037/Plate8037/ViewController.swift
+++ Plate8037/Plate8037/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var answer: Int = 0 {
+        didSet {
+            answerLabel.text = (answer == 0) ? "" : "\(answer)"
+        }
+    }
+    private var correctAnswer: Int = 0
+    
     @IBOutlet weak var firstDigitLabel: UILabel!
     @IBOutlet weak var secondDigitLabel: UILabel!
     @IBOutlet weak var thirdDigitLabel: UILabel!
     @IBOutlet weak var fourthDigitLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         if #available(iOS 13.0, *) {
@@ -25,12 +33,73 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        startAgain()
+    }
+    
+    private func startAgain() {
         let digits = Randomizer().getDigits()
-        firstDigitLabel.text = "\(digits.0)"
-        secondDigitLabel.text = "\(digits.1)"
-        thirdDigitLabel.text = "\(digits.2)"
-        fourthDigitLabel.text = "\(digits.3)"
+        let resultChecker = ResultChecker(digits: digits)
+        
+        answer = 0
+        correctAnswer = resultChecker.correctAnswer()
+        
+        firstDigitLabel.assignWithAnimation(digit: digits.0)
+        secondDigitLabel.assignWithAnimation(digit: digits.1)
+        thirdDigitLabel.assignWithAnimation(digit: digits.2)
+        fourthDigitLabel.assignWithAnimation(digit: digits.3)
+    }
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        answer *= 10
+        answer += sender.tag
+        
+        if answer == correctAnswer {
+            showAlert(title: "CONGRATULATIONS!", message: "You're right!")
+            startAgain()
+        } else if answer >= correctAnswer {
+            showAlert(title: "SORRY", message: "You're wrong, try again")
+            startAgain()
+        }
     }
 }
 
+extension UILabel {
+    func assignWithAnimation(digit: Int) {
+        let precedingNumbers = PrecedingNumbers().getNumbers(for: digit)
+        animate(precedingNumbers: precedingNumbers)
+    }
+    
+    private func animate(precedingNumbers: [Int]) {
+        transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        alpha = .zero
+        var precedingNumbers = precedingNumbers
+        if let nextNumber = precedingNumbers.first {
+            self.text = "\(nextNumber)"
+            precedingNumbers = Array(precedingNumbers.dropFirst())
+            
+            let animationDuration: TimeInterval = 0.2 + Double(arc4random() % 3) / 10
+            UIView.animate(withDuration: animationDuration,
+                           animations: { [weak self] in
+                            self?.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                            self?.alpha = 1.0
+                },
+                           completion: { [weak self] _ in self?.animate(precedingNumbers: precedingNumbers) })
+        } else {
+            transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            alpha = 1.0
+        }
+    }
+}
+
+extension UIViewController {
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        self.present(alertController, animated: true) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) { [weak alertController] in
+                alertController?.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+
+}
