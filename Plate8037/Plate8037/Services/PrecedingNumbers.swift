1fedbd1b837564f578d5e00f19d554d8599c0dcf
Sergii Sukhanov <ssuhanov@gmail.com>
2023-09-19 12:09:18 +0200

    #18 migrate to xcode 14.2 (#27)
    * #18 Update indentation
    
    * #18 Clean up redundant UI tests target
    
    * #18 Update deployment target
    
    * #18 Update gitignore

--- Plate8037/Plate8037/Services/PrecedingNumbers.swift
+++ Plate8037/Plate8037/Services/PrecedingNumbers.swift
@@ -9,29 +9,29 @@
 import Foundation
 
 class PrecedingNumbers {
-    func getNumbers(for digit: Int) -> [Int] {
-        var sign = -1
-        var addition = 0
-        var addedNumber = digit - 5
-        
-        var result: [Int] = []
-        
-        for _ in 0..<4 {
-            var nextNumber = addedNumber + sign * addition
-            while nextNumber < 0 {
-                nextNumber += 10
-            }
-            
-            while nextNumber >= 10 {
-                nextNumber -= 10
-            }
-            
-            result.append(nextNumber)
-            addedNumber = nextNumber
-            
-            addition += 2
-            sign = -sign
-        }
-        return result
+  func getNumbers(for digit: Int) -> [Int] {
+    var sign = -1
+    var addition = 0
+    var addedNumber = digit - 5
+    
+    var result: [Int] = []
+    
+    for _ in 0..<4 {
+      var nextNumber = addedNumber + sign * addition
+      while nextNumber < 0 {
+        nextNumber += 10
+      }
+      
+      while nextNumber >= 10 {
+        nextNumber -= 10
+      }
+      
+      result.append(nextNumber)
+      addedNumber = nextNumber
+      
+      addition += 2
+      sign = -sign
     }
+    return result
+  }
 }
--- Plate8037/Plate8037Tests/Services/RandomizerTests.swift
+++ Plate8037/Plate8037Tests/Services/RandomizerTests.swift
@@ -10,27 +10,27 @@ import XCTest
 @testable import Plate8037
 
 class RandomizerTests: XCTestCase {
-    var instance: Randomizer!
-    
-    override func setUp() {
-        instance = Randomizer()
-    }
-    
-    func testAllDigitsAreDifferent() {
-        let result = instance.getDigits()
-        XCTAssertNotEqual(result.0, result.1, "first and second digits should be different")
-        XCTAssertNotEqual(result.0, result.2, "first and third digits should be different")
-        XCTAssertNotEqual(result.0, result.3, "first and fourth digits should be different")
-        XCTAssertNotEqual(result.1, result.2, "second and third digits should be different")
-        XCTAssertNotEqual(result.1, result.3, "second and fourth digits should be different")
-        XCTAssertNotEqual(result.2, result.3, "third and fourth digits should be different")
-    }
-    
-    func testAllDigitsAreNotZero() {
-        let result = instance.getDigits()
-        XCTAssertNotEqual(result.0, .zero, "first digit should be not a zero")
-        XCTAssertNotEqual(result.1, .zero, "second digit should be not a zero")
-        XCTAssertNotEqual(result.2, .zero, "third digit should be not a zero")
-        XCTAssertNotEqual(result.3, .zero, "fourth digit should be not a zero")
-    }
+  var instance: Randomizer!
+  
+  override func setUp() {
+    instance = Randomizer()
+  }
+  
+  func testAllDigitsAreDifferent() {
+    let result = instance.getDigits()
+    XCTAssertNotEqual(result.0, result.1, "first and second digits should be different")
+    XCTAssertNotEqual(result.0, result.2, "first and third digits should be different")
+    XCTAssertNotEqual(result.0, result.3, "first and fourth digits should be different")
+    XCTAssertNotEqual(result.1, result.2, "second and third digits should be different")
+    XCTAssertNotEqual(result.1, result.3, "second and fourth digits should be different")
+    XCTAssertNotEqual(result.2, result.3, "third and fourth digits should be different")
+  }
+  
+  func testAllDigitsAreNotZero() {
+    let result = instance.getDigits()
+    XCTAssertNotEqual(result.0, .zero, "first digit should be not a zero")
+    XCTAssertNotEqual(result.1, .zero, "second digit should be not a zero")
+    XCTAssertNotEqual(result.2, .zero, "third digit should be not a zero")
+    XCTAssertNotEqual(result.3, .zero, "fourth digit should be not a zero")
+  }
 }
