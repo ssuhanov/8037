42ce484fd51e88019b0c4ec5176573263d14bf53
Sergii Sukhanov <ssuhanov@gmail.com>
2023-09-19 18:08:59 +0200

    #24 Add steps for specific Swiftlint version installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Tools versions #
+
+swiftlint_version="0.52.4"
+
 # Helpers #
 
 install_bundler() {
@@ -17,6 +21,39 @@ install_homebrew() {
         /usr/bin/ruby -e "$(curl -fsSL  https://raw.githubusercontent.com/Homebrew/install/master/install)"
     else
         echo "Homebrew Installed ✅"
+    fi
+}
+
+tap_ios() {
+    brew update
+    brew tap ssuhanov/tap-ios git@github.com:ssuhanov/homebrew-tap-ios.git
+}
+
+tap_swiftlint() {
+    tap_ios
+    brew install ssuhanov/homebrew-tap-ios/swiftlint@${swiftlint_version}
+    brew reinstall swiftlint@${swiftlint_version} || brew link --overwrite swiftlint@${swiftlint_version}
+    echo "Check Swiftlint version..."
+    swiftlint version
+}
+
+install_swiftlint() {
+    if ! which swiftlint >& /dev/null; then
+        echo "Swiftlint is not installed. Installing Swiftlint..."
+        if which brew >& /dev/null; then
+            tap_swiftlint
+        else
+            echo "Failed to install Swiftlint. Please install manually ❌"
+            exit 1
+        fi
+    else
+        if ! swiftlint version | grep -q $swiftlint_version; then
+            echo "Invalid Swiftlint version. ${swiftlint_version} expected. Reinstalling Swiftlint..."
+            brew uninstall --force swiftlint
+            tap_swiftlint
+        else
+            echo "Found ${swiftlint_version} version of Swiftlint ✅"
+        fi
     fi
 }
 
@@ -38,6 +75,9 @@ install_bundler
 
 echo "Looking for Homebrew..."
 install_homebrew
+
+echo "Looking for Swiftlint..."
+install_swiftlint
 
 echo "💎 Installing Gems..."
 bundle install
