a1df0e76926780e5a7152fe228c40852f9562fe2
Sergii Sukhanov <ssuhanov@gmail.com>
2023-09-19 13:24:40 +0200

    #21 Add Xcode check logic

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby File.read(ruby_version_path).strip
 source "https://rubygems.org"
 
 gem 'fastlane', '~> 2.215'
+gem 'xcode-install', '~> 2.8'
 
 plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
 eval_gemfile(plugins_path) if File.exist?(plugins_path)
--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,17 @@ install_bundler() {
     fi
 }
 
+check_xcode() {
+    project_version=$(head -n 1 .xcode-version)
+    installed_versions=($(bundle exec xcversion installed))
+    selected_version=$(bundle exec xcversion selected | grep -E "Xcode \d.*" | sed "s/Xcode //")
+    if [[ "$selected_version" == "$project_version" ]]; then
+        echo "Using Xcode ${project_version} ✅"
+    else
+        echo "Xcode ${project_version} is not installed ❌"
+    fi
+}
+
 # Implementation #
 
 echo "Looking for Bundler..."
@@ -18,3 +29,8 @@ install_bundler
 
 echo "💎 Installing Gems..."
 bundle install
+
+echo "🛠 Checking Xcode version..."
+check_xcode
+
+echo "ALL DONE! ✅"
